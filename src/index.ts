ee363afc5d296691a0b9bae4100c662bf5abb095
Priyanshu Soni <priyanshusoni0927@gmail.com>
2026-05-04 00:37:51 +0530

    added jsearch-api integration for jobs listing

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { prefRouter } from "./modules/preferences/preferences.router";
-
+import { fetchJSearchJobs } from "./modules/scrapers/jsearch.scraper";
+import { ErrorHandler } from "./middlewares/error.middleware";
 const app = express();
 
 app.use(express.json());
 
 app.use("/preferences", prefRouter);
-
+app.use(ErrorHandler);
 const PORT = process.env.PORT || 3000;
+
+async function test() {
+  const jobs = await fetchJSearchJobs();
+  console.log(jobs);
+}
+
+test();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
